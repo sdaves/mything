95749d9fe92f524c7bee98b543a9faf48671c4f9
Stephen Daves <contact@stephendaves.com>
2019-12-29 15:50:32 -0500

    Update hello.py

--- mything/hello.py
+++ mything/hello.py
@@ -1,4 +1,4 @@
-# __pragma__ ('js', {}, __include__('./standalone.module.js'))
+from preact import h, Component, render
 
 class Hello(Component):
     def __init___(self):
@@ -8,12 +8,16 @@ class Hello(Component):
     def render(self):
         return h('p', {}, self.display)
     
+w = getattr(globals(), 'window', dict(HTMLElement=object))
 
-if 'window' in globals():
-    w = globals()['window']
-    class cls(w.HTMLElement):
-        def connectedCallback(self):
-            mountPoint = w.document.createElement('span')
-            self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
-            render(h(App, {'page': self.getAttribute('page')}), mountPoint)
-    w.customElements.define('mything-hello', cls, ["page"]);
+class Element(w['HTMLElement']):
+    def connectedCallback(self):
+        mountPoint = w['document'].createElement('span')
+        self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
+        render(h(App, {'page': self.getAttribute('page')}), mountPoint)
+        
+    def define(self):
+        if 'customElements' not in w: return
+        w['customElements'].define('mything-hello', Element, ["page"])
+
+Element().define()
