6c7d3ee0adc42b431b632a9fbfba918d2d1d5986
Stephen Daves <contact@stephendaves.com>
2019-12-30 11:40:15 -0500

    Update hello.py

--- mything/hello.py
+++ mything/hello.py
@@ -1,17 +1,20 @@
 from mything.preact import h, compose, withProps, withState, attach
 
 def Hello(props={'page':'Home', 'counter':0, 'setCounter':lambda: None}):
-    return h('p', {}, page)
+    return h('div', {}, [
+        props['page'], 
+        props['counter'], 
+        h('button', {'onClick': props['setCounter']}, '+1')
+    ])
 
-def HelloComposer(props={'page':'Home'}):
-    return compose(
-      withProps({'page': props['page']),
-      withState('counter', 'setCounter', 0)
-    )
+P = compose(
+    withProps({'page': 'Home'}),
+    withState('counter', 'setCounter', 0)
+)
 
 def mount(self):
     mountPoint = document.createElement('span')
     self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
-    render(h(attach(HelloComposer())(Hello), {'page': self.getAttribute('page')}), mountPoint)
+    render(h(attach(P)(Hello), {'page': self.getAttribute('page')}), mountPoint)
 
 if 'window' in globals(): defineElement('mything-hello', ['page'], mount)
