8dc3a4aab184b54a65e9d22a5a62e5dd48b53127
Stephen Daves <contact@stephendaves.com>
2019-12-31 16:29:50 -0500

    Update todos.py

--- mything/microservices/todos.py
+++ mything/microservices/todos.py
@@ -1,8 +1,24 @@
-from .lib import bottle, decouple
+import json
 
-@bottle.route('/todos')
-def index():
-    return str(dict(data=[]))
+class Todos:
+    def __init__(self, app, bottle):
+        super().__init__()
+        self._app = app
+        self._bottle = bottle
+        self._setup_run()
+        
+    def index(self):
+        return json.dumps(dict(data=[item+'='+self._bottle.request.query[item] for item in self._bottle.request.query]))
+        
+    def _setup_run(self):
+        self._app.route('/todos')(lambda: self.index())
+        self.run = self._app.run
 
 if __name__ == '__main__':
-    bottle.run()
+    from dependencies import Injector
+    import .lib.bottle as b
+    class Container(Injector):
+        todos = Todos
+        app = b.Bottle()
+        bottle = b
+    Container.todos.run()
