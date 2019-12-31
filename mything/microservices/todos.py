1ddaf5278ddb45028197c44bfc4f57f362b88a23
Stephen Daves <contact@stephendaves.com>
2019-12-31 17:02:06 -0500

    Update todos.py

--- mything/microservices/todos.py
+++ mything/microservices/todos.py
@@ -5,15 +5,20 @@ class Todos:
         super().__init__()
         self._app = app
         self._bottle = bottle
-        self._setup_run()
+        self._setup()
         
-    def index(self):
+    def list(self):
         query = self._bottle.request.query
         return json.dumps(dict(data=[item+'='+query[item] for item in query]))
-        
-    def _setup_run(self):
-        self._app.route('/todos')(lambda: self.index())
+    
+    def single(self, name):
+        return json.dumps(dict(data=[name]))
+    
+    def _setup(self):
         self.run = self._app.run
+        route = self._app.route
+        route('/todos')(self.list)
+        route('/todos/<name>')(self.single)
 
 if __name__ == '__main__':
     from dependencies import Injector
