72914f323eff43c656a44026b9ad0050fb1a2069
Stephen Daves <contact@stephendaves.com>
2020-01-01 11:11:21 -0500

    Update hello.py

--- mything/microfrontends/hello.py
+++ mything/microfrontends/hello.py
@@ -1,20 +1,21 @@
-from lib.html import h, compose, withProps, withState, attach
+class Hello:
+    def __init__(self):
+        super().__init__()
+        self.config = compose(
+            withProps({'page': 'Home'}),
+            withState('counter', 'setCounter', 0)
+        )
 
-def render(props={'page':'Home', 'counter':0, 'setCounter':lambda x: None}):
-    return h('div', {}, [
-        props['page'], 
-        props['counter'], 
-        h('button', {'onClick': lambda: props['setCounter'](props['counter']+1)}, '+1')
-    ])
+    def render(self, props={'page':'Home', 'counter':0, 'setCounter':lambda x: None}):
+        return h('div', {}, [
+            props['page'], 
+            props['counter'], 
+            h('button', {'onClick': lambda: props['setCounter'](props['counter']+1)}, '+1')
+        ])
 
-config = compose(
-    withProps({'page': 'Home'}),
-    withState('counter', 'setCounter', 0)
-)
+    def mount(self, HTMLElement):
+        mountPoint = document.createElement('span')
+        self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
+        render(h(attach(self.config)(render), {'page': self.getAttribute('page')}), mountPoint)
 
-def mount(self):
-    mountPoint = document.createElement('span')
-    self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
-    render(h(attach(config)(render), {'page': self.getAttribute('page')}), mountPoint)
-
-if 'window' in globals(): window.defineElement('mything-hello', ['page'], mount)
+if 'window' in globals(): window.defineElement('mything-hello', ['page'], lambda x: Hello().mount(x))
