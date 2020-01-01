73c7d91928c7b351aa0a86df66f86aa747adc4e4
Stephen Daves <contact@stephendaves.com>
2020-01-01 14:22:00 -0500

    Update __init__.py

--- mything/microfrontends/hello/__init__.py
+++ mything/microfrontends/hello/__init__.py
@@ -1,10 +1,10 @@
 from mything.microfrontends.core import BaseFrontend
 
+# __pragma__ ('js', '{}', 'HelloFrontend(window.CustomHtml).define()')
+
 class HelloFrontend(BaseFrontend):    
     def __init__(self, html):
         super().__init__(html, 'mything-hello', ['name'], lambda x: self.mount(x))
         
     def render(self, props={'name':'Guest'}):
         return self._html.h('span', {}, 'Hello {0}!'.format(props['name']))
-
-if 'window' in globals(): HelloFrontend(window.CustomHtml).define()
