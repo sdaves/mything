00f603bd092d4d4984e0458f9efab19fd460855c
Stephen Daves <contact@stephendaves.com>
2020-01-01 11:33:44 -0500

    Update hello.py

--- mything/microfrontends/hello.py
+++ mything/microfrontends/hello.py
@@ -1,21 +1,38 @@
-class Hello:
-    def __init__(self):
+class CustomElement:
+    def __init__(self, html):
         super().__init__()
-        self.config = compose(
-            withProps({'page': 'Home'}),
-            withState('counter', 'setCounter', 0)
+        self._html = html
+        
+    def config(self):
+        return self._html.compose()
+
+    def mount(self, element):
+        mountPoint = window.document.createElement('span')
+        element.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
+        attrs = dict()
+        for item in self.attributes:
+            attrs[item] = element.getAttribute(item)
+        custom = self._html.h(self._html.attach(self.config())(self.render), attrs)
+        self._html.render(custom, mountPoint)    
+
+class Hello(CustomElement):
+    attributes = ['page']
+    
+    def __init__(self, html):
+        super().__init__(html)
+        self._html = html
+        
+    def config(self):
+        return self._html.compose(
+            self._html.withProps({'page': 'Home'}),
+            self._html.withState('counter', 'setCounter', 0)
         )
 
     def render(self, props={'page':'Home', 'counter':0, 'setCounter':lambda x: None}):
-        return h('div', {}, [
+        return self._html.h('div', {}, [
             props['page'], 
             props['counter'], 
-            h('button', {'onClick': lambda: props['setCounter'](props['counter']+1)}, '+1')
+            self._html.h('button', {'onClick': lambda: props['setCounter'](props['counter']+1)}, '+1')
         ])
 
-    def mount(self, HTMLElement):
-        mountPoint = document.createElement('span')
-        self.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
-        render(h(attach(self.config)(render), {'page': self.getAttribute('page')}), mountPoint)
-
-if 'window' in globals(): window.defineElement('mything-hello', ['page'], lambda x: Hello().mount(x))
+if 'window' in globals(): window.defineElement('mything-hello', Hello.attributes, lambda x: Hello().mount(x))
