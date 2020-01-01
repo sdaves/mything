641f1ea3fff1aef499a189e28a0e6ec93bc10d6e
Stephen Daves <contact@stephendaves.com>
2020-01-01 12:11:04 -0500

    Delete define-elment.js

--- docs/api/js/define-elment.js
+++
@@ -1,8 +0,0 @@
-window.defineElement = function(tag, props, mount) {    
-  class cls extends HTMLElement {
-     connectedCallback() {
-       mount(this);
-     }
-   }
-   window.customElements.define(tag, cls, props);
-}
