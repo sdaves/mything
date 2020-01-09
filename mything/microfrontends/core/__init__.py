858acdfbabcc0c7da1b7844b5e441a89671a187b
Stephen Daves <contact@stephendaves.com>
2020-01-08 21:15:00 -0500

    Update __init__.py

--- mything/microfrontends/core/__init__.py
+++ mything/microfrontends/core/__init__.py
@@ -2,19 +2,6 @@
 import typing
 # __pragma__ ('noskip')
 
-def mount(html, element, mountPoint, instance, attributes):
-    attrs = dict()
-    for item in attributes:
-        attrs[item] = element.getAttribute(item)
-    custom = instance.render(attrs)
-    provider = html.h(html.ProppyProvider, {}, [custom])
-    html.render(provider, mountPoint)    
-    element.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
-
-def define(fn, tag: str, attributes: []):
-    # __pragma__ ('js', '{}', 'class cls extends HTMLElement{connectedCallback(){mount(window.CustomHtml, this, window.document.createElement("span"),fn(window.CustomHtml),attributes);}};window.customElements.define(tag, cls, attributes);')
-    return fn
-
 class IHtml:
     def h(*args):
         pass
@@ -33,3 +20,16 @@ class IHtml:
     
     def render(*args):
         pass
+
+def mount(html, element, mountPoint, instance, attributes):
+    attrs = dict()
+    for item in attributes:
+        attrs[item] = element.getAttribute(item)
+    custom = instance.render(attrs)
+    provider = html.h(html.ProppyProvider, {}, [custom])
+    html.render(provider, mountPoint)    
+    element.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
+
+def webcomponent(fn, tag: str, attributes: []):
+    # __pragma__ ('js', '{}', 'class cls extends HTMLElement{connectedCallback(){mount(window.CustomHtml, this, window.document.createElement("span"),fn(window.CustomHtml),attributes);}};window.customElements.define(tag, cls, attributes);')
+    return fn
