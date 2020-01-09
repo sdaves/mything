ca17f6ff6184b16a6fa12c7be6caf8320affdfa7
Stephen Daves <contact@stephendaves.com>
2020-01-09 09:15:15 -0500

    Update __init__.py

--- mything/microfrontends/core/__init__.py
+++ mything/microfrontends/core/__init__.py
@@ -21,21 +21,25 @@ class IHtml:
     def render(*args):
         pass
 
-def mount(html, element, mountPoint, style, instance, attributes):
-    attrs = dict()
-    for item in attributes:
-        attrs[item] = element.getAttribute(item)
-    custom = instance.render(attrs)
-    provider = html.h(html.ProppyProvider, {}, [custom])
-    html.render(provider, mountPoint)    
-    root = element.attachShadow({ 'mode': 'open' })
-    style.rel="stylesheet"
-    style.href="js/pure-min.css"
-    root.appendChild(style)
-    root.appendChild(mountPoint)
 
-def webcomponent(tag: str, attributes: [], mount=mount):
+def webcomponent(tag: str, attributes: [], customMount=None):
+    def mountWithStyle(html, element, mountPoint, style, instance, attributes):
+        attrs = dict()
+        for item in attributes:
+            attrs[item] = element.getAttribute(item)
+        custom = instance.render(attrs)
+        provider = html.h(html.ProppyProvider, {}, [custom])
+        html.render(provider, mountPoint)    
+        root = element.attachShadow({ 'mode': 'open' })
+        style.rel="stylesheet"
+        style.href="js/pure-min.css"
+        root.appendChild(style)
+        root.appendChild(mountPoint)
+
+    mount = customMount or mountWithStyle
+    
     def wrap(fn):
         # __pragma__ ('js', '{}', 'class cls extends HTMLElement{connectedCallback(){mount(window.CustomHtml, this, window.document.createElement("span"),window.document.createElement("link"),fn(window.CustomHtml),attributes);}};window.customElements.define(tag, cls, attributes);')
         return fn
+    
     return wrap
