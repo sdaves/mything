8f540c43c21c433ccd44d44fb34f8f58b5964f4d
Stephen Daves <contact@stephendaves.com>
2020-01-09 08:55:45 -0500

    Update __init__.py

--- mything/microfrontends/core/__init__.py
+++ mything/microfrontends/core/__init__.py
@@ -21,17 +21,21 @@ class IHtml:
     def render(*args):
         pass
 
-def mount(html, element, mountPoint, instance, attributes):
+def mount(html, element, mountPoint, style, instance, attributes):
     attrs = dict()
     for item in attributes:
         attrs[item] = element.getAttribute(item)
     custom = instance.render(attrs)
     provider = html.h(html.ProppyProvider, {}, [custom])
     html.render(provider, mountPoint)    
-    element.attachShadow({ 'mode': 'open' }).appendChild(mountPoint)
+    var root = element.attachShadow({ 'mode': 'open' })
+    style.rel="stylesheet"
+    style.href="js/pure-min.css"
+    root.appendChild(style)
+    root.appendChild(mountPoint)
 
 def webcomponent(tag: str, attributes: []):
     def wrap(fn):
-        # __pragma__ ('js', '{}', 'class cls extends HTMLElement{connectedCallback(){mount(window.CustomHtml, this, window.document.createElement("span"),fn(window.CustomHtml),attributes);}};window.customElements.define(tag, cls, attributes);')
+        # __pragma__ ('js', '{}', 'class cls extends HTMLElement{connectedCallback(){mount(window.CustomHtml, this, window.document.createElement("span"),window.document.createElement("link"),fn(window.CustomHtml),attributes);}};window.customElements.define(tag, cls, attributes);')
         return fn
     return wrap
