c928f133e406e31995e3a3167397016216528d35
Stephen Daves <contact@stephendaves.com>
2020-01-11 11:48:00 -0500

    Update __init__.py

--- mything/microfrontends/counter/__init__.py
+++ mything/microfrontends/counter/__init__.py
@@ -1,12 +1,3 @@
-from dependencies import Injector
 from mything.microfrontends.core import IHtml, WebComponent
 from mything.microfrontends.counter.CounterFrontend import CounterFrontend
-
- # __pragma__ ('js', '{}', 'IHtml = window.CustomHtml')
-
-class Container(Injector):
-    html = IHtml
-    frontend = CounterFrontend
-    component = WebComponent
-    
-Container.frontend # calls frontend constructor
+# __pragma__ ('js', '{}', 'CounterFrontend(window.CustomHtml, WebComponent())')
