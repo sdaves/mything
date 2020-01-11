cd7cf468070fe951a44fce5da8c26adffd5d3dbb
Stephen Daves <contact@stephendaves.com>
2020-01-11 11:49:41 -0500

    Update __init__.py

--- mything/microfrontends/hello/__init__.py
+++ mything/microfrontends/hello/__init__.py
@@ -1,12 +1,3 @@
-from dependencies import Injector
-from mything.microfrontends.core import IHtml, WebComponent
+from mything.microfrontends.core import WebComponent
 from mything.microfrontends.hello.HelloFrontend import HelloFrontend
-
- # __pragma__ ('js', '{}', 'IHtml = window.CustomHtml')
-
-class Container(Injector):
-    html = IHtml
-    frontend = HelloFrontend
-    component = WebComponent
-    
-Container.frontend # calls frontend constructor
+# __pragma__ ('js', '{}', 'HelloFrontend(window.CustomHtml, WebComponent())')
